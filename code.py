b913a32ed194c0edb9ff0dea00e063bd131ffdd9
99003740 <78853299+99003740@users.noreply.github.com>
2021-02-24 14:59:30 +0530

    was

--- code.py
+++ code.py
@@ -5,8 +5,7 @@ import re
 input_file = open("input.txt", "r")                                   # reading the input file
 count = 0
 input_search = input("Enter the Word to be search:\n")                # user input requried word
-input_file2 = input_file.read()
-word=re.findall(input_search, input_file.read(), re.M|re.I)         # finding all the user input requried word from the input file
+input_file2 = input_file.read()        # finding all the user input requried word from the input file
 input_file1 = input_file2.split()
 user_input_search_word = input_search + '.txt'
 user_input_file = open(user_input_search_word, "w+")                              # counting the number of times input word is repeating
@@ -14,7 +13,8 @@ for i in range(len(input_file1)):
     word = re.match(input_search, input_file1[i], re.M | re.I)         # finding all the user input required word from the input file
     if word:
         count += 1
-        user_input_file.write(input_file1[i-1]+' '+input_file1[i]+' '+input_file1[i+1]+'\n')
+        user_input_file.write(input_file1[i-1]+' '+input_file1[i]+' '+input_file1[i+1]+'\n'+'\n')
 
 
 user_input_file.write("Number of times word appered in file:  " + str(count))
+
