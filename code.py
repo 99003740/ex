96ca0c825007b67f5da3e60f5c3c59361a471de4
99003740 <78853299+99003740@users.noreply.github.com>
2021-02-25 12:54:10 +0530

    single class

--- code.py
+++ code.py
@@ -1,24 +1,33 @@
 # Author: Rajendra L Hanagodi
 # Date of creation:23/02/2021
 
+
+
 import re
-input_file = open("input.txt", "r")                                   # reading the input file
-count = 0
-input_search = input("Enter the Word to be search:\n")                # user input requried word
-input_file2 = input_file.read()        # finding all the user input requried word from the input file
-input_file1 = input_file2.split()
-input_file3 = re.split(r'\W+',str(input_file1))
-user_input_search_word = input_search + '.txt'
-user_input_file = open(user_input_search_word, 'w')                              # counting the number of times input word is repeating
-for i in range(len(input_file1)):
-    word = re.match(input_search, input_file1[i], re.M | re.I)         # finding all the user input required word from the input file
-    if word:
-        count += 1
-        user_input_file.write(input_file1[i-1]+' '+input_file1[i]+' '+input_file1[i+1]+'\n'+'\n')
+class search_word:
 
+    def __init__(self):
+        print("This is init")
 
-user_input_file.write("Number of times word appered in file:  " + str(count))
+    def operation(self):
+        input_file = open("input.txt", "r")                                   
+        count = 0
+        input_search = input("Enter the Word to be search:\n")                
+        input_file2 = input_file.read()        
+        input_file1 = input_file2.split()
+        input_file3 = re.split(r'\W',str(input_file1))
 
-input_file.close()
-user_input_file.close()
+        user_input_search_word = input_search + '.txt'
 
+        user_input_file = open(user_input_search_word, 'w')                              
+        for i in range(len(input_file1)):
+            word = re.match(input_search, input_file1[i], re.M | re.I)         
+            if word:
+                count += 1
+                user_input_file.write(input_file1[i-1]+' '+input_file1[i]+' '+input_file1[i+1]+'\n'+'\n')
+        user_input_file.write("Number of times word appered in file:  " + str(count))
+        input_file.close()
+        user_input_file.close()
+
+obj=search_word()
+obj.operation()
